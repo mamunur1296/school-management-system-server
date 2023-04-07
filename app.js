b5b1ef28eb8fa2609920ea15426e01e5f1718c0f
mamunur1296 <programer597@gmail.com>
2023-04-07 21:12:27 +0600

    initial app setap ok

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+//external import
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const {
+  isNotFoundHandler,
+  isErrorhandler,
+} = require("./meddlewars/common/errorHandler");
+//internal import
 const app = express();
-app.use(express.application.json());
+dotenv.config();
+
+//Mongodb connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connection successful"))
+  .catch((err) => console.log(err));
+// request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//parser cookies
+app.use(cookieParser(process.env.COOKIE_SECRETE));
+//routing
+
+//404 error handel
+app.use(isNotFoundHandler);
+//common error handler
+app.use(isErrorhandler);
+
+//app listen
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`application is ran on port ${process.env.PORT}`);
+});
